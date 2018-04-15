c37535625fc9d331711e89b9c9ce766698253b11
Saúl Cabrera <saulecabrera@gmail.com>
2018-04-15 18:48:19 -0400

    Add hexdoc + package info

--- mix.exs
+++ mix.exs
@@ -1,28 +1,38 @@
 defmodule Leibniz.MixProject do
   use Mix.Project
 
+  @desc "Math expression parser and evaluator"
+
   def project do
     [
       app: :leibniz,
       version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @desc,
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package() do
+    [
+      name: "leibniz",
+      maintainers: ["Saúl Cabrera"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/saulecabrera/leibniz"}
+    ]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 end
